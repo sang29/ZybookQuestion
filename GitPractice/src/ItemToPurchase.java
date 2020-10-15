6abe5456e113fb0127d0d99908997f1ce362441a
sangikhan <sangikhan@levy-ve703-00386.apn.wlan.private.upenn.edu>
2020-10-15 13:14:48 -0400

    Add getters and setters for vars

--- GitPractice/src/ItemToPurchase.java
+++ GitPractice/src/ItemToPurchase.java
@@ -1,4 +1,27 @@
 
 public class ItemToPurchase {
+	
+	String itemName;
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public int getItemPrice() {
+		return itemPrice;
+	}
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+	int itemPrice;
+	int itemQuantity;
+	
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
 
 }
